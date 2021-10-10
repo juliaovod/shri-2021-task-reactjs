15b19df3a2bd91c2a2e11a77a9442b7dbee19a45
juliaovod <ovoddev@gmail.com>
2021-10-10 09:03:52 +0300

    Move redux to other branch

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createBrowserHistory } from 'history';
-import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import App from '@/App';
-import createStore from '@/store';
 
 import 'UiKit/theme/index.css';
 
 const browserHistory = createBrowserHistory();
 
 const createApp = (): void => {
-  const store = createStore();
-
   ReactDOM.render(
-    <Provider store={store}>
-      <Router history={browserHistory}>
-        <App />
-      </Router>
-    </Provider>,
+    <Router history={browserHistory}>
+      <App />
+    </Router>,
     document.getElementById('root'),
   );
 };

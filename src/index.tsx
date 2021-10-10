5ab08b784e5917f000e44e5532f0761328e7a4ab
juliaovod <ovoddev@gmail.com>
2021-10-10 20:50:41 +0300

    Add and init redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import App from '@/App';
+import createStore from '@/store';
 
 import 'UiKit/theme/index.css';
 
 const browserHistory = createBrowserHistory();
 
 const createApp = (): void => {
+  const store = createStore();
+
   ReactDOM.render(
-    <Router history={browserHistory}>
-      <App />
-    </Router>,
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <App />
+      </Router>
+    </Provider>,
     document.getElementById('app-root'),
   );
 };
